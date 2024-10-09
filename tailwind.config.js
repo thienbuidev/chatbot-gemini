adbb5003906cd27544b47841d5147e079685a73a
thien <thien@192.168.0.110>
2024-10-09 20:49:12 +0700

    init repo

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        gemLight: "#B3D9F2",   /* Light Blue */
+        gemRegular: "#99c2d9", /* Medium Blue */
+        gemDeep: "#317fa3",    /* Deep Blue */
+      }
+    },
+  },
+  plugins: [],
+}
+
